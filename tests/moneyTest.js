002b361bccd9471a183d000603911fa05862742f
Jeff Morgan <jefferyantwi95@gmail.com>
2025-11-10 10:25:27 +0000

    Remove default tests and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -7,20 +7,19 @@ if(formatCurrency(2095) === '20.95'){
     console.log('Passed');
 }
 else{
-    console.log('Failed')
+    console.log('Failed');
 }
 
 
 console.log('Works with zero');
 if(formatCurrency(0) === '0.00'){
-    console.log('passed')
+    console.log('passed');
 }
 else{
     console.log('failed');
 }
 
-
-console.log('Rounds up to the nearest cents')
+console.log('Rounds up to the nearest cents');
 if(formatCurrency(2000.5) === '20.01'){
     console.log('passed');
 }
