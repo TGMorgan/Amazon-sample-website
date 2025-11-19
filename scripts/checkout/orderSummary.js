90cfdc400f4452be7a3585e3aa1244b62dc060a5
Jeff Morgan <jefferyantwi95@gmail.com>
2025-11-19 22:59:02 +0000

    Create automated tests for  orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -9,8 +9,6 @@ import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary(){
 
-  displayQuantityOnHeader();
-
   let cartSummaryHTML = '';
   cart.forEach((cartItem)=>{
       let productId = cartItem.productId;
@@ -24,7 +22,7 @@ export function renderOrderSummary(){
       let dateString = deliveryDate.format('dddd, MMMM D');
 
       let html = `
-          <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+          <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Delivery date: ${dateString}
               </div>
@@ -40,7 +38,7 @@ export function renderOrderSummary(){
                   <div class="product-price">
                     $${(formatCurrency(matchingProduct.priceCents))}
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                       Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -51,7 +49,7 @@ export function renderOrderSummary(){
                       <input type="text" class="quantity-input js-quantity-input js-quantity-input-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                       <span class="save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
