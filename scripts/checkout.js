addf0a93804451e8474df48158883bb3ed9e2657
Jeff Morgan <jefferyantwi95@gmail.com>
2026-02-14 12:27:58 +0000

    Convert loadProducts from XMLHttpRequest to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,27 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 /* import '../data/cart-class.js'; */
 /* import '../data/car.js'; */
 /* import '../data/backend-practice.js'; */
 
+Promise.all([
+    loadProductsFetch(),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then((values)=>{
+    console.log(values);
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+/*
 Promise.all([
     new Promise((resolve)=>{
         loadProducts(()=>{
@@ -23,7 +38,7 @@ Promise.all([
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
-})
+}) */
 
 /* new Promise((resolve)=>{
     loadProducts(()=>{
